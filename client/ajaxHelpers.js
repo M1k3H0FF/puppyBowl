38f9e1d6d49783279b72850fc56fb7da13654e14
Carlos <chnava@me.com>
2022-07-01 18:15:42 -0500

    was able to get button to work

--- client/ajaxHelpers.js
+++ client/ajaxHelpers.js
@@ -1,34 +1,30 @@
 // Add your cohort name to the cohortName variable below, replacing the 'COHORT-NAME' placeholder
-const cohortName = '2206-FTB-ET-WEB-FT';
+const cohortName = "2206-FTB-ET-WEB-FT";
 // Use the APIURL variable for fetch requests
 const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
 
-
 export const fetchAllPlayers = async () => {
-    try {
-        const response = await fetch(`${APIURL}/players`);
-        const result = await response.json();
-        if (result.error) throw result.error;
-        return result.data.players;
-    } catch (err) {
-        console.error('Uh oh, trouble fetching players!', err);
-    }
-
+  try {
+    const response = await fetch(`${APIURL}/players`);
+    const result = await response.json();
+    if (result.error) throw result.error;
+    return result.data.players;
+  } catch (err) {
+    console.error("Uh oh, trouble fetching players!", err);
+  }
 };
 
 export const fetchSinglePlayer = async (playerId) => {
-    try {
-        const data  = await fetch(`/api/${cohortName}/players/${playerId}`);
-        return data;
-    } catch (err) {
-        console.log('error', err);
-    }
+  try {
+    const data = await fetch(`${APIURL}/players/${playerId}`, {});
+    const result = await data.json();
+    if (result.error) throw result.error;
+    return result.data.player;
+  } catch (error) {
+    console.error("error");
+  }
 };
 
-export const addNewPlayer = async (playerObj) => {
+export const addNewPlayer = async (playerObj) => {};
 
-};
-
-export const removePlayer = async (playerId) => {
-
-};
+export const removePlayer = async (playerId) => {};
